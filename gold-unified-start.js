5b6fa604ebc1ad4b50ce7bd4649e9a42b882d2f8
shalaanalshalaan1000-dotcom <shalaanalshalaan1000@gmail.com>
2026-09-21 13:57:47 +0300

    Tag deployed runtime as ICT top-down liquidity build

--- gold-unified-start.js
+++ gold-unified-start.js
@@ -1,7 +1,7 @@
 import http from 'node:http';
 import { spawn } from 'node:child_process';
 
-const BUILD_TAG='site-signal-noai-v14-protected-entry';
+const BUILD_TAG='ict-topdown-v15-liquidity-targets';
 const PORT=Number(process.env.PORT||3000);
 const UI_PORT=3001,AUTO_PORT=3002,DATA_PORT=3005;
 
