6bf4f240352182879bdefe9e26b999de1b01be3a
shalaanalshalaan1000-dotcom <shalaanalshalaan1000@gmail.com>
2026-09-26 18:37:32 +0300

    Tag unified runtime as confluence engine

--- gold-unified-start.js
+++ gold-unified-start.js
@@ -1,7 +1,7 @@
 import http from 'node:http';
 import { spawn } from 'node:child_process';
 
-const BUILD_TAG='ict-origin-liquidity-v20-stateful-continuation';
+const BUILD_TAG='confluence-v1-stateful';
 const PORT=Number(process.env.PORT||3000);
 const UI_PORT=3001,AUTO_PORT=3002,DATA_PORT=3005;
 
